e5013949c5ecf21ebac5b99db25296cc903dc21e
tobi <76791368+2bits2@users.noreply.github.com>
2025-01-05 19:53:28 +0100

    moved system programs to user programs

--- .config/tobi/system/configuration.nix
+++ .config/tobi/system/configuration.nix
@@ -113,44 +113,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-
      home-manager # manage user installed software
      stow         # manage dotfile configuration
-
-     # basic functionality
-     neovim       # best editor
-     alacritty    # terminal
-     xclip        # put something into clipboard
-     fzf          # fuzzy find
-     killall
-     unzip        # uncompress files
-     tmux         # yea terminal stuff
-     zoxide       # nicer cd
-     bat          # cat with colors
-     eza          # like ls
-     ripgrep      # searching files by text
      vscode       # needed for coding
-     jq           # json parsing
-     dunst        # displaying desktop notifications
-     libnotify    # sending desktop notifications
-     keepassxc    # passwords
-
-     pywal        # set the wallpaper and color scheme
-     picom        # window transparency and stuff
-
-     git          # version control
-     gh           # git interacting with github
-
-     gnumake      # for building dependencies
-     gcc          # compiler
-
-     nodejs       # frontend development
-
-     xdotool      # get the title of a window
-     wmctrl       # rename active window
-     rofi         # window switcher
-
-
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- .config/tobi/system/home-manager/home.nix
+++ .config/tobi/system/home-manager/home.nix
@@ -2,9 +2,39 @@
 {
   home = {
     packages = with pkgs; [
-      hello
-      cowsay
-      lolcat
+
+      # basic functionality
+      neovim       # best editor
+      alacritty    # terminal
+      xclip        # put something into clipboard
+      fzf          # fuzzy find
+      killall
+      unzip        # uncompress files
+      tmux         # yea terminal stuff
+      zoxide       # nicer cd
+      bat          # cat with colors
+      eza          # like ls
+      ripgrep      # searching files by text
+      jq           # json parsing
+      dunst        # displaying desktop notifications
+      libnotify    # sending desktop notifications
+      keepassxc    # passwords
+
+      pywal        # set the wallpaper and color scheme
+      picom        # window transparency and stuff
+
+      git          # version control
+      gh           # git interacting with github
+
+      gnumake      # for building dependencies
+      gcc          # compiler
+
+      nodejs       # frontend development
+
+      xdotool      # get the title of a window
+      wmctrl       # rename active window
+      rofi         # window switcher
+
     ];
 
     # This needs to actually be set to your username
